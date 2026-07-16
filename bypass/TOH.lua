72a3fe3ad849fbe2740d5bdb51601287b6cb54cb
bari_vcd <140895716+bari-vcd@users.noreply.github.com>
2026-07-17 00:13:18 +0300

    Create TOH.lua

---
+++ bypass/TOH.lua
@@ -0,0 +1,36 @@
+
+-------------------------------------------
+--[[
+	
+	Scripted by @vcd_
+	
+	Last Updated: 17/07/26
+
+  Tower of Hell Anti Cheat Bypass
+	
+--]]
+-------------------------------------------
+
+local cloneref = (cloneref or function<T>(reference: T): T return reference end)
+local st       = cloneref(game:GetService('StarterPlayer'))
+local player   = cloneref(game:GetService('Players').LocalPlayer)
+
+Destroy  = game.Destroy
+ls1, ls2 = player.PlayerScripts.LocalScript, player.PlayerScripts.LocalScript2
+
+for _, v in pairs(getconnections(ls1.Changed)) do
+    v:Disable()
+end
+
+for _, v in pairs(getconnections(st.StarterPlayerScripts.LocalScript.Changed)) do
+    v:Disable()
+end
+
+ls1.Enabled, ls2.Enabled = false, false
+t1, t2 = getscriptthread(ls1), getscriptthread(ls2);
+coroutine.close(t1)
+coroutine.close(t2)
+Destroy(ls1)
+Destroy(ls2)
+Destroy(st.StarterPlayerScripts.jump)
+Destroy(st.StarterPlayerScripts.AnchorPlayer)
