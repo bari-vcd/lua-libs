c3953f4d737729a5bc4ab8fdbb8d99f4282dbf38
bari_vcd <140895716+bari-vcd@users.noreply.github.com>
2026-09-14 20:42:50 +0300

    Create simple-signal.lua

---
+++ simple-signal.lua
@@ -0,0 +1,28 @@
+--!strict
+
+local Signal   = {}
+Signal.__index = Signal
+
+function Signal.new()
+	local self = setmetatable({
+		_handlers = {}
+	}, Signal)
+	return self
+end
+
+function Signal:Connect(fn)
+	table.insert(self._handlers, fn)
+	return {
+		Disconnect = function()
+			for i, v in ipairs(self._handlers) do
+				if v == fn then table.remove(self._handlers, i) break end
+			end
+		end
+	}
+end
+
+function Signal:Fire(...)
+	for _, fn in ipairs(self._handlers) do
+		task.spawn(fn, ...)
+	end
+end
